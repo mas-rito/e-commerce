2cfbadaf82a08c649db607cd8d730900c9616d37
Rito Ramadhan <126365429+mas-rito@users.noreply.github.com>
2023-12-08 15:20:14 +0700

    feat: promotion section

--- src/app/(pages)/[slug]/page.tsx
+++ src/app/(pages)/[slug]/page.tsx
@@ -21,6 +21,7 @@ import { generateMeta } from '../../_utilities/generateMeta'
 export const dynamic = 'force-dynamic'
 
 import Categories from '../../_components/Categories'
+import Promotion from '../../_components/Promotion'
 
 import classes from './index.module.scss'
 
@@ -64,6 +65,7 @@ export default async function Page({ params: { slug = 'home' } }) {
           <Hero {...hero} />
           <Gutter className={classes.home}>
             <Categories categories={categories} />
+            <Promotion />
           </Gutter>
         </section>
       ) : (
--- src/app/_components/Categories/CategoryCard/index.tsx
+++ src/app/_components/Categories/CategoryCard/index.tsx
@@ -1,7 +1,10 @@
+'use client'
+
 import React from 'react'
 import Link from 'next/link'
 
 import { Category, Media } from '../../../../payload/payload-types'
+import { useFilter } from '../../../_providers/Filter'
 
 import classess from './index.module.scss'
 
@@ -11,12 +14,16 @@ type CategoryCardProps = {
 
 const CategoryCard = ({ category }: CategoryCardProps) => {
   const media = category.media as Media
+  const { setCategoryFilters } = useFilter()
 
   return (
     <Link
       href={'/products'}
       className={classess.card}
       style={{ backgroundImage: `url(${media.url})` }}
+      onClick={() => {
+        setCategoryFilters([category.id])
+      }}
     >
       <p className={classess.title}>{category.title}</p>
     </Link>
